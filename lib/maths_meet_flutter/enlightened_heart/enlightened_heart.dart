75d9970ba32aeee6ad0e62818b672fdb0cd4fb67
rutvik110 <takrutvik@gmail.com>
2022-05-04 22:55:31 +0530

    perfectly looping heart achieved

--- lib/maths_meet_flutter/enlightened_heart/enlightened_heart.dart
+++ lib/maths_meet_flutter/enlightened_heart/enlightened_heart.dart
@@ -62,22 +62,37 @@ class EnlightenedHeartPainter extends CustomPainter {
     const angle = pi * 2 / 360;
     for (var i = 0; i < 360; i++) {
       final itemValue = i / 360;
-      final isInRange =
-          itemValue > animationValue - 0.5 && itemValue < animationValue + 0.1;
+      //calculate the angle of point
+      double pointAngleValue = angle * i;
+      double startAngle = angleValue;
+      double endAngle = angleValue + angle * 45;
+
+      //check if pointAngleValue lies within start and end angle
+
+      bool isInRange =
+          pointAngleValue >= startAngle && pointAngleValue <= endAngle;
+
+      if (!isInRange) {
+        if (angleValue >= angle * 315) {
+          isInRange = pointAngleValue + angle * 360 >= startAngle &&
+              pointAngleValue + angle * 360 <= endAngle;
+        }
+      }
 
       if (isInRange) {
+        // dev.log((i % 45).toString());
+        final radius = ((i % 45) / 45) * 0.3;
+        // dev.log(radius.toString());
         canvas.drawCircle(
           Offset(
-            16 * pow(sin(angle * i), 3) * 15,
+            16 * pow(sin(pointAngleValue), 3) * 15,
             -15 *
-                (13 * cos(angle * i) -
-                    5 * cos(angle * i * 2) -
-                    2 * cos(angle * i * 3) -
-                    cos(angle * i * 4)),
-            // cos(angle * i) * 100,
-            // sin(angle * i) * 100,
+                (13 * cos(pointAngleValue) -
+                    5 * cos(pointAngleValue * 2) -
+                    2 * cos(pointAngleValue * 3) -
+                    cos(pointAngleValue * 4)),
           ),
-          itemValue - animationValue + 0.3,
+          0.2, // radius == 0 ? 0.1 : radius + 0.1,
           smallcircles,
         );
       }
@@ -91,10 +106,27 @@ class EnlightenedHeartPainter extends CustomPainter {
 
     for (var i = 0; i < 45; i++) {
       final itemValue = i / 45;
-      final isInRange =
-          itemValue > animationValue - 0.1 && itemValue < animationValue + 0.1;
-      final shouldFlicker = Random().nextBool();
+      //calculate the angle of point
+      double pointAngleValue = angle2 * i;
+      double startAngle = angleValue;
+      double endAngle = angleValue + angle * 45;
+
+      //check if pointAngleValue lies within start and end angle
+
+      bool isInRange =
+          pointAngleValue >= startAngle && pointAngleValue <= endAngle;
+
+      if (!isInRange) {
+        if (angleValue >= angle2 * 315) {
+          isInRange = pointAngleValue + angle * 360 >= startAngle &&
+              pointAngleValue + angle * 360 <= endAngle;
+        }
+      }
+      // final isInRange =
+      //     itemValue > animationValue - 0.1 && itemValue < animationValue + 0.1;
       if (isInRange) {
+        final shouldFlicker = Random().nextBool();
+
         canvas.drawCircle(
           Offset(
             16 * pow(sin(angle2 * i), 3) * 15,
@@ -103,10 +135,8 @@ class EnlightenedHeartPainter extends CustomPainter {
                     5 * cos(angle2 * i * 2) -
                     2 * cos(angle2 * i * 3) -
                     cos(angle2 * i * 4)),
-            // cos(angle * i) * 100,
-            // sin(angle * i) * 100,
           ),
-          itemValue - animationValue + 0.8 + (shouldFlicker ? 0.5 : 0),
+          itemValue - animationValue + (shouldFlicker ? 0.9 : 0),
           smallcircles2,
         );
       }
@@ -119,8 +149,24 @@ class EnlightenedHeartPainter extends CustomPainter {
 
     for (var i = 0; i < 10; i++) {
       final itemValue = i / 10;
-      final isInRange =
-          itemValue > animationValue - 0.1 && itemValue < animationValue + 0.1;
+      //calculate the angle of point
+      double pointAngleValue = angle3 * i;
+      double startAngle = angleValue;
+      double endAngle = angleValue + angle * 45;
+
+      //check if pointAngleValue lies within start and end angle
+
+      bool isInRange =
+          pointAngleValue >= startAngle && pointAngleValue <= endAngle;
+
+      if (!isInRange) {
+        if (angleValue >= angle3 * 315) {
+          isInRange = pointAngleValue + angle * 360 >= startAngle &&
+              pointAngleValue + angle * 360 <= endAngle;
+        }
+      }
+      // final isInRange =
+      //     itemValue > animationValue - 0.1 && itemValue < animationValue + 0.1;
       final shouldFlicker = Random().nextBool();
       if (isInRange) {
         canvas.drawCircle(
@@ -131,8 +177,6 @@ class EnlightenedHeartPainter extends CustomPainter {
                     5 * cos(angle3 * i * 2) -
                     2 * cos(angle3 * i * 3) -
                     cos(angle3 * i * 4)),
-            // cos(angle * i) * 100,
-            // sin(angle * i) * 100,
           ),
           3 + (shouldFlicker ? 0.7 : 0),
           smallcircles3,
@@ -142,9 +186,9 @@ class EnlightenedHeartPainter extends CustomPainter {
 
     final activeCircle = Paint()
       ..style = PaintingStyle.fill
-      ..color = Colors.lightBlueAccent;
+      ..color = Colors.blue;
     for (var i = 0; i < 5; i++) {
-      final updatedAngle = angleValue + 0.22 + (i / 10);
+      final updatedAngle = angleValue + 0.4 + (i / 10);
       canvas.drawCircle(
         Offset(
           16 * pow(sin(updatedAngle), 3) * 15,
@@ -154,7 +198,7 @@ class EnlightenedHeartPainter extends CustomPainter {
                   2 * cos(updatedAngle * 3) -
                   cos(updatedAngle * 4)),
         ),
-        i.toDouble(),
+        i.toDouble() / 2,
         activeCircle,
       );
     }
