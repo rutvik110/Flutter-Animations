37af63e6fd4e7eaf950f751b3b636a1a3ae2d314
rutvik110 <takrutvik@gmail.com>
2022-05-05 18:34:53 +0530

    refactored

--- lib/maths_meet_flutter/enlightened_heart/enlightened_heart.dart
+++ lib/maths_meet_flutter/enlightened_heart/enlightened_heart.dart
@@ -52,156 +52,175 @@ class EnlightenedHeartPainter extends CustomPainter {
   });
   final double animationValue;
   final double angleValue;
+
   @override
   void paint(Canvas canvas, Size size) {
-    final smallcircles = Paint()
-      ..style = PaintingStyle.fill
-      ..color = Colors.white;
     canvas.translate(size.width / 2, size.height / 2);
 
-    const angle = pi * 2 / 360;
+    final angle = calculateAngleBasedOnPoints(360);
+    final visibleArc = angle * 45;
+    final edgeCaseAngle = 360 * angle - visibleArc;
+    double startAngle = angleValue;
+
+    double endAngle = angleValue + visibleArc;
+    const double heartRadius = 15;
+
+    // Drawing group of circles which represent the  visible portion of hearts arc as animation progresses
+    final circlesGroup1 = Paint()
+      ..style = PaintingStyle.fill
+      ..color = Colors.white;
+
     for (var i = 0; i < 360; i++) {
-      final itemValue = i / 360;
       //calculate the angle of point
       double pointAngleValue = angle * i;
-      double startAngle = angleValue;
-      double endAngle = angleValue + angle * 45;
 
       //check if pointAngleValue lies within start and end angle
 
-      bool isInRange =
-          pointAngleValue >= startAngle && pointAngleValue <= endAngle;
-
+      bool isInRange = checkIfPointInRange(
+        pointAngleValue: pointAngleValue,
+        startAngle: startAngle,
+        endAngle: endAngle,
+      );
       if (!isInRange) {
-        if (angleValue >= angle * 315) {
-          isInRange = pointAngleValue + angle * 360 >= startAngle &&
-              pointAngleValue + angle * 360 <= endAngle;
+        if (checkIfAngleGreaterThanEdgeCase(angle, edgeCaseAngle)) {
+          isInRange = checkIfPointInRange(
+            pointAngleValue: pointAngleValue + angle * 360,
+            startAngle: startAngle,
+            endAngle: endAngle,
+          );
         }
       }
 
       if (isInRange) {
-        // dev.log((i % 45).toString());
-        final radius = ((i % 45) / 45) * 0.3;
-        // dev.log(radius.toString());
         canvas.drawCircle(
-          Offset(
-            16 * pow(sin(pointAngleValue), 3) * 15,
-            -15 *
-                (13 * cos(pointAngleValue) -
-                    5 * cos(pointAngleValue * 2) -
-                    2 * cos(pointAngleValue * 3) -
-                    cos(pointAngleValue * 4)),
-          ),
-          0.2, // radius == 0 ? 0.1 : radius + 0.1,
-          smallcircles,
+          calculatePointOffset(angle: pointAngleValue, radius: heartRadius),
+          0.2,
+          circlesGroup1,
         );
       }
     }
 
-    final smallcircles2 = Paint()
+    // Drawing flickering small blue circles on the heart
+
+    final circlesGroup2 = Paint()
       ..style = PaintingStyle.fill
       ..color = Colors.blue;
 
-    const angle2 = pi * 2 / 45;
+    final angle2 = calculateAngleBasedOnPoints(45);
 
     for (var i = 0; i < 45; i++) {
-      final itemValue = i / 45;
       //calculate the angle of point
       double pointAngleValue = angle2 * i;
-      double startAngle = angleValue;
-      double endAngle = angleValue + angle * 45;
 
       //check if pointAngleValue lies within start and end angle
 
-      bool isInRange =
-          pointAngleValue >= startAngle && pointAngleValue <= endAngle;
-
+      bool isInRange = checkIfPointInRange(
+        pointAngleValue: pointAngleValue,
+        startAngle: startAngle,
+        endAngle: endAngle,
+      );
       if (!isInRange) {
-        if (angleValue >= angle2 * 315) {
-          isInRange = pointAngleValue + angle * 360 >= startAngle &&
-              pointAngleValue + angle * 360 <= endAngle;
+        if (checkIfAngleGreaterThanEdgeCase(angle2, edgeCaseAngle)) {
+          isInRange = checkIfPointInRange(
+            pointAngleValue: pointAngleValue + angle * 360,
+            startAngle: startAngle,
+            endAngle: endAngle,
+          );
         }
       }
-      // final isInRange =
-      //     itemValue > animationValue - 0.1 && itemValue < animationValue + 0.1;
+
+      if (true) {
+        final shouldFlicker = Random().nextBool();
+
+        canvas.drawCircle(
+          calculatePointOffset(angle: angle2 * i, radius: heartRadius),
+          1 - animationValue + (shouldFlicker ? 0.9 : 0),
+          circlesGroup2,
+        );
+      }
+    }
+
+    // Drawing flickering large blue circles on the heart
+
+    final circlesGroup3 = Paint()
+      ..style = PaintingStyle.fill
+      ..color = Colors.blue;
+
+    final angle3 = calculateAngleBasedOnPoints(10);
+
+    for (var i = 0; i < 10; i++) {
+      //calculate the angle of point
+      double pointAngleValue = angle3 * i;
+
+      //check if pointAngleValue lies within start and end angle
+
+      bool isInRange = checkIfPointInRange(
+        pointAngleValue: pointAngleValue,
+        startAngle: startAngle,
+        endAngle: endAngle,
+      );
+
+      if (!isInRange) {
+        if (checkIfAngleGreaterThanEdgeCase(angle3, edgeCaseAngle)) {
+          isInRange = checkIfPointInRange(
+            pointAngleValue: pointAngleValue + angle * 360,
+            startAngle: startAngle,
+            endAngle: endAngle,
+          );
+        }
+      }
+
       if (isInRange) {
         final shouldFlicker = Random().nextBool();
 
         canvas.drawCircle(
-          Offset(
-            16 * pow(sin(angle2 * i), 3) * 15,
-            -15 *
-                (13 * cos(angle2 * i) -
-                    5 * cos(angle2 * i * 2) -
-                    2 * cos(angle2 * i * 3) -
-                    cos(angle2 * i * 4)),
-          ),
-          itemValue - animationValue + (shouldFlicker ? 0.9 : 0),
-          smallcircles2,
-        );
-      }
-    }
-    final smallcircles3 = Paint()
-      ..style = PaintingStyle.fill
-      ..color = Colors.blue;
-
-    const angle3 = pi * 2 / 10;
-
-    for (var i = 0; i < 10; i++) {
-      final itemValue = i / 10;
-      //calculate the angle of point
-      double pointAngleValue = angle3 * i;
-      double startAngle = angleValue;
-      double endAngle = angleValue + angle * 45;
-
-      //check if pointAngleValue lies within start and end angle
-
-      bool isInRange =
-          pointAngleValue >= startAngle && pointAngleValue <= endAngle;
-
-      if (!isInRange) {
-        if (angleValue >= angle3 * 315) {
-          isInRange = pointAngleValue + angle * 360 >= startAngle &&
-              pointAngleValue + angle * 360 <= endAngle;
-        }
-      }
-      // final isInRange =
-      //     itemValue > animationValue - 0.1 && itemValue < animationValue + 0.1;
-      final shouldFlicker = Random().nextBool();
-      if (isInRange) {
-        canvas.drawCircle(
-          Offset(
-            16 * pow(sin(angle3 * i), 3) * 15,
-            -15 *
-                (13 * cos(angle3 * i) -
-                    5 * cos(angle3 * i * 2) -
-                    2 * cos(angle3 * i * 3) -
-                    cos(angle3 * i * 4)),
-          ),
+          calculatePointOffset(angle: angle3 * i, radius: heartRadius),
           3 + (shouldFlicker ? 0.7 : 0),
-          smallcircles3,
+          circlesGroup3,
         );
       }
     }
 
-    final activeCircle = Paint()
+    // Drawing group of circles which move along the  visible portion of hearts arc as animation progresses
+
+    final circlesGroup4 = Paint()
       ..style = PaintingStyle.fill
       ..color = Colors.blue;
-    for (var i = 0; i < 5; i++) {
-      final updatedAngle = angleValue + 0.4 + (i / 10);
+    for (var i = 0; i < 45; i++) {
+      final updatedAngle = angleValue + 2.6 + (i / 10);
       canvas.drawCircle(
-        Offset(
-          16 * pow(sin(updatedAngle), 3) * 15,
-          -15 *
-              (13 * cos(updatedAngle) -
-                  5 * cos(updatedAngle * 2) -
-                  2 * cos(updatedAngle * 3) -
-                  cos(updatedAngle * 4)),
-        ),
-        i.toDouble() / 2,
-        activeCircle,
+        calculatePointOffset(angle: updatedAngle, radius: heartRadius),
+        i.toDouble() * 0.03,
+        circlesGroup4,
       );
     }
+  }
+
+  bool checkIfPointInRange({
+    required double pointAngleValue,
+    required double startAngle,
+    required double endAngle,
+  }) {
+    return pointAngleValue >= startAngle && pointAngleValue <= endAngle;
+  }
+
+  bool checkIfAngleGreaterThanEdgeCase(double angle, double edgeCaseAngle) {
+    return angleValue >= edgeCaseAngle; //angle * 315;
+  }
+
+  double calculateAngleBasedOnPoints(int points) {
+    return pi * 2 / points;
+  }
+
+  Offset calculatePointOffset({required double angle, required double radius}) {
+    return Offset(
+      16 * pow(sin(angle), 3) * radius,
+      -radius *
+          (13 * cos(angle) -
+              5 * cos(angle * 2) -
+              2 * cos(angle * 3) -
+              cos(angle * 4)),
+    );
   }
 
   @override
