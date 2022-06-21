b9567007cea195ccf6103117af02639e8f9cc82d
rutvik110 <takrutvik@gmail.com>
2022-06-21 16:09:23 +0530

    some refactoring over implementation

--- lib/flutter_design_challenges/macos_inspired_doc/macos_inspired_doc.dart
+++ lib/flutter_design_challenges/macos_inspired_doc/macos_inspired_doc.dart
@@ -12,53 +12,60 @@ class MacOsInspiredDoc extends StatefulWidget {
 class _MacOsInspiredDocState extends State<MacOsInspiredDoc> {
   late int? hoveredIndex;
   late double baseItemHeight;
+  late double baseTranslationY;
   late double verticlItemsPadding;
-
   double getScaledSize(int index) {
-    if (hoveredIndex == null) {
-      return baseItemHeight;
-    }
-
-    late final double hoveredSize;
-    final difference = (hoveredIndex! - index).abs();
-    // deciedes how many items from the currently hovered index will be affected
-    // if changing also adjust hoveredSize accordingly values
-    final itemsAffected = items.length;
-    if (difference == 0) {
-      hoveredSize = 70;
-    } else if (difference <= itemsAffected) {
-      final scaleRatio = (items.length - difference) / itemsAffected;
-
-      hoveredSize = lerpDouble(baseItemHeight, 50, scaleRatio)!;
-    } else {
-      hoveredSize = baseItemHeight;
-    }
-
-    return hoveredSize;
+    return getPropertyValue(
+      index: index,
+      baseValue: baseItemHeight,
+      maxValue: 70,
+      nonHoveredMaxValue: 50,
+    );
   }
 
   double getTranslationY(int index) {
+    return getPropertyValue(
+      index: index,
+      baseValue: baseTranslationY,
+      maxValue: -22,
+      nonHoveredMaxValue: -14,
+    );
+  }
+
+  double getPropertyValue({
+    required int index,
+    required double baseValue,
+    required double maxValue,
+    required double nonHoveredMaxValue,
+  }) {
+    // 1.
     if (hoveredIndex == null) {
-      return 0;
+      return baseValue;
     }
 
-    late final double translationY;
-
+    late final double propertyValue;
+    // 2.
     final difference = (hoveredIndex! - index).abs();
-    // deciedes how many items from the currently hovered index will be affected
-    // if changing also adjust translation accordingly
-    final itemsAffected = items.length;
-    if (difference == 0) {
-      translationY = -22;
-    } else if (difference <= itemsAffected) {
-      final scaleRatio = (items.length - difference) / itemsAffected;
 
-      translationY = lerpDouble(0, -14, scaleRatio)!;
+    // 3.
+    final itemsAffected = items.length;
+
+    // 4.
+    if (difference == 0) {
+      propertyValue = maxValue;
+
+      // 5.
+    } else if (difference <= itemsAffected) {
+      final ratio = (itemsAffected - difference) / itemsAffected;
+
+      propertyValue = lerpDouble(baseValue, nonHoveredMaxValue, ratio)!;
+
+      // 6.
     } else {
-      translationY = 0;
+      propertyValue = baseValue;
     }
 
-    return translationY;
+    return propertyValue;
   }
 
   @override
@@ -67,99 +74,90 @@ class _MacOsInspiredDocState extends State<MacOsInspiredDoc> {
     super.initState();
     hoveredIndex = null;
     baseItemHeight = 40;
+    baseTranslationY = 0;
     verticlItemsPadding = 10;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        //explain how stack sizes it self
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            // constraintes passed down by positioned.fill
-            Positioned.fill(
-              //TODO: explain why we had to use align
-              // and what's difference between center and align
-              //could referance the positioned widget video on the docs page for Positioned
-              child: Align(
-                child: SizedBox(
-                  height: baseItemHeight,
-                  width: double.maxFinite,
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      gradient: const LinearGradient(colors: [
-                        Colors.blueAccent,
-                        Colors.greenAccent,
-                      ]),
-                    ),
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              // constraintes passed down by positioned.fill
+              //doesnt know where to put its children
+              Positioned(
+                height: baseItemHeight,
+                left: 0,
+                right: 0,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    gradient: const LinearGradient(colors: [
+                      Colors.blueAccent,
+                      Colors.greenAccent,
+                    ]),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(verticlItemsPadding),
-              //explain use of intrinsic width and why it's expensive
-              // as layout is calculated in advance to know the child size
-              // and this not one pass and expensive
-              child: IntrinsicWidth(
+
+              Padding(
+                padding: EdgeInsets.all(verticlItemsPadding),
                 child: Row(
+                    mainAxisSize: MainAxisSize.min,
                     children: List.generate(items.length, (index) {
-                  return MouseRegion(
-                    onEnter: ((event) {
-                      setState(() {
-                        hoveredIndex = index;
-                      });
-                    }),
-                    onExit: (event) {
-                      setState(() {
-                        hoveredIndex = null;
-                      });
-                    },
-                    child: AnimatedContainer(
-                      duration: const Duration(
-                        milliseconds: 300,
-                      ),
-                      transform: Matrix4.identity()
-                        ..translate(
-                          0.0,
-                          getTranslationY(index),
-                          0.0,
-                        ),
-                      height: getScaledSize(index),
-                      width: getScaledSize(index),
-                      // color: Colors.green,
-                      alignment: AlignmentDirectional.bottomCenter,
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 10,
-                      ),
-                      //explain the use of fittedbox
-                      child: FittedBox(
-                        fit: BoxFit.contain,
-                        child: AnimatedDefaultTextStyle(
+                      return MouseRegion(
+                        cursor: SystemMouseCursors.click,
+                        onEnter: ((event) {
+                          setState(() {
+                            hoveredIndex = index;
+                          });
+                        }),
+                        onExit: (event) {
+                          setState(() {
+                            hoveredIndex = null;
+                          });
+                        },
+                        child: AnimatedContainer(
                           duration: const Duration(
                             milliseconds: 300,
                           ),
-                          style: TextStyle(
-                            fontSize: getScaledSize(index),
+                          transform: Matrix4.identity()
+                            ..translate(
+                              0.0,
+                              getTranslationY(index),
+                              0.0,
+                            ),
+                          height: getScaledSize(index),
+                          width: getScaledSize(index),
+
+                          alignment: AlignmentDirectional.bottomCenter,
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 10,
                           ),
-                          child: Text(
-                            items[index],
+                          //explain the use of fittedbox
+                          child: FittedBox(
+                            child: AnimatedDefaultTextStyle(
+                              duration: const Duration(
+                                milliseconds: 300,
+                              ),
+                              style: TextStyle(
+                                fontSize: getScaledSize(index),
+                              ),
+                              child: Text(
+                                items[index],
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                  );
-                }).toList()),
+                      );
+                    }).toList()),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
+            ],
+          ),
+        ));
   }
 }
 
