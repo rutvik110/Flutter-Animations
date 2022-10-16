e8858895a078685696816b5729814d5e7051ceb3
rutvik110 <takrutvik@gmail.com>
2022-10-16 21:54:31 +0530

    stripes shader rotation math updated

--- lib/flutter_shaders/stripes_shader/README.md
+++ lib/flutter_shaders/stripes_shader/README.md
@@ -1,2 +1,12 @@
 # Stripes Shader
 https://user-images.githubusercontent.com/65209850/196029076-c6548842-cbab-4632-b79f-fc3b2cde9150.mov
+
+# Supporting Article
+
+## [Create Mind-Blowing Visuals Using Shaders in Flutter](https://takrutvik.com/create-mind-blowing-visuals-using-shaders-in-flutter)
+
+### Compile Shader
+
+```
+umbra generate lib/flutter_shaders/stripes_shader/stripes.glsl --output lib/flutter_shaders/stripes_shader/
+```
--- lib/flutter_shaders/stripes_shader/stripes.glsl
+++ lib/flutter_shaders/stripes_shader/stripes.glsl
@@ -6,15 +6,27 @@ uniform float direction;
 uniform float warpScale;
 uniform float warpTiling;
 
+vec2 rotatePoint(vec2 pt,vec2 center,float angle){
+    float sinAngle=sin(angle);
+    float cosAngle=cos(angle);
+    pt-=center;
+    vec2 r=vec2(1.);
+    r.x=pt.x*cosAngle-pt.y*sinAngle;
+    r.y=pt.x*sinAngle+pt.y*cosAngle;
+    r+=center;
+    return r;
+}
+
 vec4 fragment(in vec2 uv,in vec2 fragCoord){
     // vec2 uv=gl_FragCoord.xy/u_resolution.xy;
     
     // float tiles=tiles;
     
-    vec2 uv2=uv;
+    // vec2 uv2=uv;
     
-    uv2.x=mix(uv.x,uv.y,direction);
-    uv2.y=mix(uv.y,1.-uv.x,direction);
+    // uv2.x=mix(uv.x,uv.y,direction);
+    // uv2.y=mix(uv.y,1.-uv.x,direction);
+    vec2 uv2=rotatePoint(uv.xy,vec2(.5,.5),direction*2.*PI);
     
     uv2.x+=sin(uv2.y*warpTiling*PI*2.)*warpScale+speed;//u_time/10.;
     uv2.x*=tiles;
