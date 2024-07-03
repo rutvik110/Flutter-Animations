1b13dcc24fc295c55c4d1e89f98b21f96ea1a5b6
Rutvik Tak <65209850+rutvik110@users.noreply.github.com>
2024-07-04 00:13:06 +0530

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animations/custom_painters/blobs.dart';
+import 'package:flutter_animations/flutter_design_challenges/pictures_stack/pictures_stack.dart';
 import 'package:flutter_animations/flutter_gestures/continous_drag_selection/continous_drag_selection.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      isToolbarVisible: false,
+      builder: (context) => const MyApp(), // Wrap your app
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -19,7 +27,17 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         // textTheme: GoogleFonts.rubikTextTheme(Theme.of(context).textTheme),
       ),
-      home: const BlobsView(),
+      builder: DevicePreview.appBuilder,
+      home: const PicturesStack(),
     );
+  }
+}
+
+class ShaderStripesDemo extends StatelessWidget {
+  const ShaderStripesDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
